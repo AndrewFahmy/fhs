e70d70de92bb0a83a31f3408ae2ee72e72d4b4d0
Andrew M. Fahmy <andrew.fahmy@outlook.com>
2026-08-29 22:59:15 +0300

    feat: Refactor actor data initialization to use constants for subject and actor IDs

--- backend/FHS.Api/Constants/DataConstants.cs
+++ backend/FHS.Api/Constants/DataConstants.cs
@@ -3,6 +3,12 @@ public static partial class AppConstants
     public static class Data
     {
         public const string DatabaseConnectionName = "DbConnection";
+
+        public const string LineOperatorSubjectId = "11111111-1111-4111-8111-111111111111";
+        public static readonly Guid LineOperatorActorId = Guid.Parse("a5a5a5a5-0000-4000-8000-000000000001");
+        public const string AdminSubjectId = "22222222-2222-4222-8222-222222222222";
+        public static readonly Guid AdminActorId = Guid.Parse("a5a5a5a5-0000-4000-8000-000000000002");
+
         public const int ActorDisplayNameMaxLength = 200;
         public const int ActorSubjectIdMaxLength = 255;
         public const int DefectDescriptionMaxLength = 500;
--- backend/FHS.Api/Data/Configuration/ActorConfiguration.cs
+++ backend/FHS.Api/Data/Configuration/ActorConfiguration.cs
@@ -21,14 +21,14 @@ public sealed class ActorConfiguration : IEntityTypeConfiguration<Actor>
         builder.HasData(
             new Actor
             {
-                Id = Guid.Parse("a5a5a5a5-0000-4000-8000-000000000001"),
-                SubjectId = "11111111-1111-4111-8111-111111111111",
+                Id = AppConstants.Data.LineOperatorActorId,
+                SubjectId = AppConstants.Data.LineOperatorSubjectId,
                 DisplayName = "Line Operator"
             },
             new Actor
             {
-                Id = Guid.Parse("a5a5a5a5-0000-4000-8000-000000000002"),
-                SubjectId = "22222222-2222-4222-8222-222222222222",
+                Id = AppConstants.Data.AdminActorId,
+                SubjectId = AppConstants.Data.AdminSubjectId,
                 DisplayName = "System Administrator",
             }
         );
