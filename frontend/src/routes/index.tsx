9a7f4ded9e9b0900ddc0eb29e486bd0edd381169
Andrew M. Fahmy <andrew.fahmy@outlook.com>
2026-09-06 22:39:47 +0300

    removing frontend project and to rebuild it. Adding old project folder to gitignore file

--- frontend/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/")({
-    component: Home,
-});
-
-function Home() {
-    return <h1 className="text-2xl font-semibold">Fault Handling System</h1>;
-}
